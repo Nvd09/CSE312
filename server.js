d5fe6812557fb6e31719a57fd9849978052f9cb2
Aritra-Paul <aritrapa@buffalo.edu>
2020-04-19 23:39:00 -0400

    User Login

--- server.js
+++ server.js
@@ -4,24 +4,31 @@ Express is used for request handling. Documemntation - >https://expressjs.com/en
 path is used for acessing files. Documentation -> https://nodejs.org/api/path.html
 
 */
-var express = require('express');
+const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+
+const app = express();
+
+// set view engine
+app.set('view engine', 'ejs');
+
+// set up routes
+app.use('/auth', authRoutes);
+
+// create home route
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 
+// app.use("/profile", static('user_profile.html'));
+// app.use("/signin", static('welcome.html'));
+// app.use("/chat", static('chatui.html'));
+// app.use("/homepage.html", static('homepage.html'));
+// app.use("/chatui.html", static('chatui.html'));
+// app.use("/user_profile.html", static('user_profile.html'));
 
-var app = express();
-
-app.use(express.static('public'));
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/welcome.html");
-})
-app.use("/profile", express.static('user_profile.html'));
-app.use("/signin", express.static('welcome.html'));
-app.use("/chat", express.static('chatui.html'));
-app.use("/homepage.html", express.static('homepage.html'));
-app.use("/chatui.html", express.static('chatui.html'));
-app.use("/user_profile.html", express.static('user_profile.html'));
-
-app.listen(8000, function() {
-    console.log("Server is running on Port: " + 8000);
+app.listen(3000, function() {
+    console.log("Server is running on Port: " + 3000);
 });
 
