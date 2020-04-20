805cf59babd0938788359c7528a13a6aec54cd17
abdurrahman75 <abdur.rahman@email.com>
2020-04-20 13:11:55 -0400

    update server.js

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ path is used for acessing files. Documentation -> https://nodejs.org/api/path.ht
 
 */
 var express = require('express');
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 const multer = require('multer');
 var path = require('path');
 var mongoose = require('mongoose');
@@ -20,13 +19,10 @@ var storage = multer.diskStorage({
  
     cb(null, 'image'+ '-' + Date.now() )
   }
-=======
-=======
-
->>>>>>> 7f061ba97d01188465615d2fdb3e500afba3c40c
 
 
->>>>>>> 7f061ba97d01188465615d2fdb3e500afba3c40c
+
+
 
 })
  
@@ -48,8 +44,7 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/welcome.html");
 })
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 
 app.post('/upload-image', upload.single('profile-picture'), (req, res) => {
@@ -79,19 +74,17 @@ app.post('/upload-image', upload.single('profile-picture'), (req, res) => {
 
 app.use("/profile", express.static('user_profile.html'));
 
-=======
 app.use("/profile", express.static('user_profile.html'));
->>>>>>> 7f061ba97d01188465615d2fdb3e500afba3c40c
-=======
+
+
 app.use("/profile", express.static('user_profile.html'));
->>>>>>> 7f061ba97d01188465615d2fdb3e500afba3c40c
+
 app.use("/signin", express.static('welcome.html'));
 app.use("/chat", express.static('chatui.html'));
 app.use("/homepage.html", express.static('homepage.html'));
 app.use("/chatui.html", express.static('chatui.html'));
 app.use("/user_profile.html", express.static('user_profile.html'));
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 app.get('/retrieve-image', (req, res) => {
  Image.find(function(err, images) {
         if (err) {
@@ -103,10 +96,7 @@ app.get('/retrieve-image', (req, res) => {
  
 
 });
-=======
->>>>>>> 7f061ba97d01188465615d2fdb3e500afba3c40c
-=======
->>>>>>> 7f061ba97d01188465615d2fdb3e500afba3c40c
+
 
 app.listen(8000, function() {
     console.log("Server is running on Port: " + 8000);
